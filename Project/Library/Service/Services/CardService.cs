f30b04bce36ad9ba1483facc225d0594fcdb0681
rozophat <nguyenthanhphat18@gmail.com>
2016-03-31 08:47:18 +0700

    commit4

---
+++ Project/Library/Service/Services/CardService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Root.Data.Infrastructure;
+
+namespace Service.Services
+{
+	public interface ICardService
+	{
+		void SaveCard();
+	}
+
+	public class CardService : ICardService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public CardService(IUnitOfWork unitOfWork)
+		{
+			this._unitOfWork = unitOfWork;
+		}
+		public void SaveCard()
+		{
+			_unitOfWork.Commit();
+		}
+	}
+}
---
+++ Project/Library/Service/Services/PlayerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Root.Data.Infrastructure;
+
+namespace Service.Services
+{
+	public interface IPlayerService
+	{
+		void SavePlayer();
+	}
+
+	public class PlayerService: IPlayerService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public PlayerService(IUnitOfWork unitOfWork)
+		{
+			this._unitOfWork = unitOfWork;
+		}
+		public void SavePlayer()
+		{
+			_unitOfWork.Commit();
+		}
+	}
+}
